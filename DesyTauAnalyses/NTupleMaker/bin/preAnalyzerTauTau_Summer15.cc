b544dfa79d3e9c80a8e871f8fba8c72cf744eef7
Aruna Nayak <Aruna.Nayak@cern.ch>
2015-09-03 14:30:58 +0200

    update

--- DesyTauAnalyses/NTupleMaker/bin/preAnalyzerTauTau_Summer15.cc
+++ DesyTauAnalyses/NTupleMaker/bin/preAnalyzerTauTau_Summer15.cc
@@ -30,7 +30,7 @@
 //#include "TauAnalysis/CandidateTools/interface/candidateAuxFunctions.h"
 //#include "TauAnalysis/CandidateTools/interface/neuralMtautauAuxFunctions.h"
 //#include "BtagSF.hh" 
-#include "readJSONFile.h"
+//#include "readJSONFile.h"
 
 #include "Math/Vector3D.h"
 #include "Math/Vector4D.h"
@@ -105,6 +105,93 @@ enum MCEVENTTYPE{HADHAD, LEPHAD, LEPLEP, LL, LNU, LEPNU, TAUNU, OTHERS};
 enum BVariation{kNo = 0, kDown = 1, kUp = 2};
 //BtagSF* btsf = new BtagSF(12345);
 
+//Reading json file and filter lumi section
+std::map<int, std::vector<std::pair<int, int> > >
+readJSONFile(const std::string& inFileName)
+{
+  std::ifstream inFile(inFileName.c_str(), std::ios::in);
+  
+  std::string line;
+  while(!inFile.eof())
+    {
+      std::string buffer;
+      inFile >> buffer;
+      line += buffer;
+    }
+  
+  // define map with result
+  std::map<int, std::vector<std::pair<int, int> > > jsonMap;
+  
+  // loop on JSON file
+  for(std::string::const_iterator it = line.begin(); it < line.end(); ++it)
+    {
+      // find run number
+      if( (*(it) == '"') && (*(it+7) == '"') )   
+        {
+	  std::string run(it+1, it+7);
+          //std::cout << "found run " << run << std::endl;
+	  
+          // find lumi sections
+	  std::vector<std::pair<int, int> > lumisections;
+          for(std::string::const_iterator it2 = it+10; it2 < line.end(); ++it2)
+            {
+              if( (*(it2) == ']') && (*(it2-1) == ']') ) break;
+              if( *(it2) != '[' ) continue;
+              
+	      std::string::const_iterator it_beg = it2;
+	      std::string::const_iterator it_mid;
+	      std::string::const_iterator it_end;
+	      
+              for(std::string::const_iterator it3 = it_beg; it3 < line.end(); ++it3)
+                {
+                  if( *(it3) == ',' ) it_mid = it3;
+                  if( *(it3) == ']' )
+                    {
+                      it_end = it3;
+                      break;
+                    }
+                }
+	      std::string lumi_beg(it_beg+1, it_mid);
+	      std::string lumi_end(it_mid+1, it_end);
+              //std::cout << "[" << lumi_beg;
+              //std::cout << ",";
+              //std::cout << lumi_end << "]" << std::endl;
+              
+	      std::pair<int, int> tempLS(atoi(lumi_beg.c_str()), atoi(lumi_end.c_str()));
+              lumisections.push_back(tempLS);
+              
+              it2 = it_end;
+            }
+          
+          jsonMap[atoi(run.c_str())] = lumisections;
+        } // find run number
+      
+    } // loop on JSON file
+  
+  return jsonMap;
+}
+
+bool AcceptEventByRunAndLumiSection(const int& runId, const int& lumiId,
+                                    std::map<int, std::vector<std::pair<int, int> > >& jsonMap)
+{
+  // select by runId
+  if( jsonMap.find(runId) == jsonMap.end() ) return false;
+  
+  // select by lumiId
+  std::vector<std::pair<int, int> > lumisections = jsonMap[runId];
+  
+  int skipEvent = true;
+  for(unsigned int i = 0; i < lumisections.size(); ++i)
+    if( (lumiId >= lumisections.at(i).first) &&
+        (lumiId <= lumisections.at(i).second) )
+      skipEvent = false;
+  
+  if( skipEvent == true ) return false;
+  
+  return true;
+}
+
+//compute DeltaR between two vectors
 double deltaR(LV v1, LV v2) {
 
   double deta = v1.Eta() - v2.Eta();
@@ -114,6 +201,7 @@ double deltaR(LV v1, LV v2) {
 
 }
 
+//Weights for N-Jet MC samples
 float reweightHEPNUPWJets(int hepNUP) {
 
   int nJets = hepNUP-5;
@@ -173,7 +261,7 @@ int getJetIDMVALoose(double pt, double eta, double rawMVA)
   }
   return passId;
 }
-
+//Remove duplication of events in ntuples
 bool checkEventIsDuplicated(MAPDITAU_run &mapDiTau, int run, int lumi, int event, float ptL1, float ptL2, float etaL1, float etaL2)
 {
   
